82fc881b105f66ac0d910b63e7152ed2fc1b1920
Ruggero <infiniteproject@gmail.com>
2016-05-05 13:45:12 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+env
+
+set -x
+if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
+    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
+    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
+    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_ADMIN" ]; then
+    sed -i "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN<\/admin>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_CLIENTS" ]; then
+    sed -i "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_CLIENTS<\/clients>/g" /etc/icecast2/icecast.xml
+fi
+
+cat /etc/icecast2/icecast.xml
