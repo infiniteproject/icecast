afabdff588f8c31333d778794b0e8fbf546ceca6
Ruggero <infiniteproject@gmail.com>
2016-05-05 03:58:24 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+env
+set -x
+
+if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
+    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
+    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
+    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast2/icecast.xml
+fi
+if [ -n "$ICECAST_PASSWORD" ]; then
+    sed -i "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast2/icecast.xml
+fi
+
+cat /etc/icecast2/icecast.xml
+supervisord -n -c /etc/supervisord.conf
